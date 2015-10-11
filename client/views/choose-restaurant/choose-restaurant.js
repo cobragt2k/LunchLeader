3f21a7d851d8ec701d702f7412b3ab40cf378fe3
Barry Wong <bwong@barrymwong.com>
2015-10-10 17:56:14 -0700

    added search from yelp api and click on item routes to leaderboard page

--- client/views/choose-restaurant/choose-restaurant.js
+++ client/views/choose-restaurant/choose-restaurant.js
@@ -1,10 +1,36 @@
+var restaurantSearch = function(event, template) {
+  event.preventDefault();
+
+  var restaurantName = $("#restaurant-input").val();
+
+  var yelpSearchText = $('#restaurant-input').val();
+
+  Meteor.call('getYelpSearchResults',yelpSearchText, function(err, response){
+    Session.set('yelpResults', response);
+    console.log(err);
+  });
+
+};
+
 Template.chooseRestaurant.events({
   "click #add-restaurant-button": function(event, template) {
+    restaurantSearch(event, template);
+  },
+  "submit form": function(event, template) {
+    restaurantSearch(event, template);
+  }
+});
+
+Template.chooseRestaurant.yelpResults = function(){
+    return Session.get('yelpResults').businesses || "Please search...";
+};
+
+Template.chooseRestaurant.events({
+  "click .yelp-result-li": function (event, template) {
     event.preventDefault();
-    var restaurantName = $("#restaurant-input").val();
 
     var restaurantId = Restaurants.insert({
-      name: restaurantName,
+      yelpData: this,
       votes: []
     });
 
@@ -14,7 +40,8 @@ Template.chooseRestaurant.events({
     meal.restaurantIds.push(restaurantId);
 
     MealSessions.update(mealId, meal);
-    return Router.go("/meal/" + mealId + "/leaderboard");
 
+    Session.set('yelpResults', '');
+    return Router.go("/meal/" + mealId + "/leaderboard");
   }
-})
+});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,49 @@
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Meteor.methods({
+
+  getYelpSearchResults: function(search) {
+  // Query OAUTH credentials (these are set manually)
+  //var auth = Accounts.loginServiceConfiguration.findOne({service: 'yelp'});
+
+  // Add auth signature manually
+  //auth['serviceProvider'] = { signatureMethod: "HMAC-SHA1" };
+
+    var auth = {
+        consumerSecret: "hK9gvz41glWwSSGqyN3lviLKnwo", // "nZw3_mkpUTs7hFWiLrxQAtE2dPo",
+        consumerKey: "g2405YRES-FgILfUHV0GDg",// "2Vt-kngU-JGLCVuQBxi0pA"
+        accessToken: "J5iwfqtMxutjMMq6p6FiR_L4UzawJlPS",
+        accessTokenSecret: "gdAqrZD0sVHR_yTu9Tlb2sxbyho"
+    };
+
+    var parameters = {};
+
+    parameters.term = search;
+
+    // (SF is default location)
+    parameters.location = 'San+Francisco';
+
+    // Results limited to 5
+    parameters.limit = 5;
+
+    // Configure OAUTH parameters for REST call
+    parameters.oauth_consumer_key = auth.consumerKey;
+    parameters.oauth_consumer_secret = auth.consumerSecret;
+    parameters.oauth_token = auth.accessToken;
+    parameters.oauth_signature_method = "HMAC-SHA1";
+
+    var config = {
+      consumerKey: auth.consumerKey,
+      secret: auth.consumerSecret
+    };
+
+    var urls = {
+      requestToken: 'http://api.yelp.com/v2/search',
+      accessToken: auth.accessToken
+    }
+    // Create OAUTH1 headers to make request to Yelp API
+    var oauthBinding = new OAuth1Binding(config, urls);
+    oauthBinding.accessTokenSecret = auth.accessTokenSecret;
+    var headers = oauthBinding._buildHeader();
+    return oauthBinding._call('GET', 'http://api.yelp.com/v2/search', headers, parameters).data;
+  }
+
+});
